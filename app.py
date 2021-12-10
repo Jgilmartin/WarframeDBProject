c14901cc75f9553d779666571b4dad8e4278b92c
Berith <92763166+Berith1@users.noreply.github.com>
2021-12-09 16:10:50 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ctypes import sizeof
 from sqlite3 import dbapi2
 import PySimpleGUI as sg
 import sqlite3 as db
@@ -9,53 +8,35 @@ conn = db.connect('warframeproject.sqlite')
 conn.row_factory = lambda cursor, row: row[0]
 c = conn.cursor()
 allLoadouts = c.execute('SELECT l_name FROM loadouts').fetchall()
-
 primaryWeapons = c.execute('SELECT wp_name FROM weaponsP').fetchall()
 primaryWeaponsDamageTypes = c.execute('SELECT wp_DamageTypes FROM weaponsP').fetchall()
 primaryWeaponsFireRate = c.execute('SELECT wp_fireRate FROM weaponsP').fetchall()
 primaryWeaponsFireTypes = c.execute('SELECT wp_fireType FROM weaponsP').fetchall()
 primaryWeaponsNoise = c.execute('SELECT wp_noise FROM weaponsP').fetchall()
-
 secondaryWeapons = c.execute('SELECT wp_name FROM weaponsS').fetchall()
 secondaryWeaponsDamageTypes = c.execute('SELECT wp_DamageTypes FROM weaponsS').fetchall()
 secondaryWeaponsFireRate = c.execute('SELECT wp_fireRate FROM weaponsS').fetchall()
 secondaryWeaponsFireTypes = c.execute('SELECT wp_fireType FROM weaponsP').fetchall()
 secondaryWeaponsNoise = c.execute('SELECT wp_noise FROM weaponsP').fetchall()
-
 meleeWeapons = c.execute('SELECT wp_name FROM weaponsM').fetchall()
 meleeWeaponsDamageTypes = c.execute('SELECT wp_DamageTypes FROM weaponsS').fetchall()
 meleeWeaponsFireRate = c.execute('SELECT wp_fireRate FROM weaponsS').fetchall()
 meleeWeaponsFireTypes = c.execute('SELECT wp_fireType FROM weaponsP').fetchall()
 meleeWeaponsNoise = c.execute('SELECT wp_noise FROM weaponsP').fetchall()
 warframeNames = c.execute('SELECT DISTINCT wf_name FROM warframe').fetchall()
+warframeId = c.execute('SELECT wf_id FROM warframe').fetchall()
+warframeArmor = c.execute('SELECT wf_armor FROM warframe_armor').fetchall()
+warframeEnergy = c.execute('SELECT wf_energy FROM warframe_energy').fetchall()
+warframeHealth = c.execute('SELECT wf_health FROM warframe_health').fetchall()
+warframeRelease = c.execute('SELECT wf_release FROM warframe_release').fetchall()
+warframeShields = c.execute('SELECT wf_shields FROM warframe_shields').fetchall()
+warframeSpeed = c.execute('SELECT wf_speed FROM warframe_speed').fetchall()
 
-loadoutNames = c.execute('SELECT l_name FROM loadouts').fetchall()
-loadoutPrimaries = c.execute('SELECT l_primaryWeapon FROM loadouts').fetchall()
-loadoutSecondaries = c.execute('SELECT l_secondaryWeapon FROM loadouts').fetchall()
-loadoutMelee = c.execute('SELECT l_meleeWeapon FROM loadouts').fetchall()
-loadoutWarframe = c.execute('SELECT l_warframe FROM loadouts').fetchall()
-result = c.execute('SELECT * FROM weaponsS').fetchall()
+#nameConversion = c.execute('SELECT wf_id FROM warframe').fetchall()
 
-
-filterSelections = []
-for i in range(0,13):
-    filterSelections.append(False)
-print (filterSelections)
-
-def resetFilterView():
-    c.execute('DROP TABLE filterView')
-    c.execute(""" 
-    CREATE TABLE filterView AS 
-    SELECT * FROM weaponsM
-    UNION 
-    SELECT * FROM weaponsP
-    UNION 
-    SELECT * FROM weaponsS;""")
-    conn.commit()
+allWeapons = meleeWeapons + primaryWeapons + secondaryWeapons
 
 
-#, , loadoutSecondaries, loadoutMelee, loadoutWarframe]
-loadoutViewHeadings = 'Loadout Name       Primary Weapon      Secondary Weapon    Melee Weapon    Warframe'
 
 # ----------- Create the 3 layouts this Window will display -----------
 layout1 = [[sg.Text('Loadout Select')],*[[sg.Combo(allLoadouts, key = 'allLoadouts', enable_events=True)]], 
@@ -120,63 +101,35 @@ layout4 = [[sg.Text('Melee Weapon Editor ')],
            #[sg.Button('AddWaframe')]]
 
 layout5 = [[sg.Text('Warframe Editor ')],
-           [sg.Text('Primary Weapons ')],
-           *[[sg.Combo(primaryWeapons, key = 'editedWeaponP', enable_events=True)]],
-           [sg.Text("Damage type:", size = (15,1)), sg.Input(key='DamageTypeP')],
-           [sg.Text("Fire rate:", size = (15,1)), sg.Input(key='fireRateP')],
-           [sg.Text("Fire type:", size = (15,1)), sg.Input(key='fireTypeP')],
-           [sg.Text("Noise type:", size = (15,1)), sg.Input(key='noiseP')],
-           [sg.Button('UpdateWeaponStatsPrimary')],
-           [sg.Text('Secondary Weapons ')],
-           *[[sg.Combo(secondaryWeapons, key = 'editedWeaponS', enable_events=True)]],
-           [sg.Text("Damage type:", size = (15,1)), sg.Input(key='DamageTypeS')],
-           [sg.Text("Fire rate:", size = (15,1)), sg.Input(key='fireRateS')],
-           [sg.Text("Fire type:", size = (15,1)), sg.Input(key='fireTypeS')],
-           [sg.Text("Noise type:", size = (15,1)), sg.Input(key='noiseS')],
-           [sg.Button('UpdateWeaponStatsSecondary')],
-           [sg.Text('Melee Weapons ')],
-           *[[sg.Combo(meleeWeapons, key = 'editedWeaponM', enable_events=True)]],
-           [sg.Text("Damage type:", size = (15,1)), sg.Input(key='DamageTypeM')],
-           [sg.Text("Fire rate:", size = (15,1)), sg.Input(key='fireRateM')],
-           [sg.Text("Fire type:", size = (15,1)), sg.Input(key='fireTypeM')],
-           [sg.Text("Noise type:", size = (15,1)), sg.Input(key='noiseM')],
-           [sg.Button('UpdateWeaponStatsMelee')],
+           [sg.Text('Warframes ')],
+           #[sg.Text("Legend: 1=", size = (15,1))],
+           *[[sg.Combo(warframeId, key = 'editedWarframe', enable_events=True)]],
+           [sg.Text("Armor Value:", size = (15,1)), sg.Input(key='armor')],
+           [sg.Button('UpdateWarframeArmor')],
+           [sg.Text("Energy Value:", size = (15,1)), sg.Input(key='energy')],
+           [sg.Button('UpdateWarframeEnergy')],
+           [sg.Text("Health Value:", size = (15,1)), sg.Input(key='health')],
+           [sg.Button('UpdateWarframeHealth')],
+           [sg.Text("Release Date:", size = (15,1)), sg.Input(key='release')],
+           [sg.Button('UpdateWarframeRelease')],
+           
+           [sg.Text("Shield Value:", size = (15,1)), sg.Input(key='shield')],
+           [sg.Button('UpdateWarframeShields')],
+           [sg.Text("Speed Value:", size = (15,1)), sg.Input(key='speed')],
+           [sg.Button('UpdateWarframeSpeed')]]
+           
            #[sg.Button('UpdateWeapon')],
            #[sg.Button('AddWeapon')],
 
-           [sg.Text('Warframe Editor ')],
-           *[[sg.Combo(warframeNames, key = 'editedWarframe', enable_events=True)]]]
+           
            #[sg.Text("Enter new value:", size = (15,1)), sg.Input(key='warframeEdit')],
            #[sg.Button('UpdateWarframe')],
            #[sg.Button('AddWaframe')]]
 
+testList = [['string', 'another string'], [2]]
+testHeadings = ['Heading 1', 'Heading 2']
 
-
-layout6 = [[sg.Text('Loadout Viewer', key = 'test1')], 
-            [sg.Text(loadoutViewHeadings)],
-            [sg.Listbox(loadoutNames, size= (15,30) ,no_scrollbar=True), sg.Listbox(loadoutPrimaries, size= (15,30), no_scrollbar=True, key = 'l_primaries'), 
-            sg.Listbox(loadoutSecondaries, size= (15,30), no_scrollbar=True, key = 'l_secondaries'), sg.Listbox(loadoutMelee, size= (15,30), no_scrollbar=True, key = 'l_melee'), sg.Listbox(loadoutWarframe, size= (15,30), no_scrollbar=True, key = 'l_warframes')],
-            ]
-
-#--------------CREATE FILTERS-----------------
-filNames = c.execute('SELECT wp_name from filterView').fetchall()
-filClass = c.execute('SELECT wp_class from filterView').fetchall()
-filDamageTypes = c.execute('SELECT wp_DamageTypes from filterView').fetchall()
-filFireRate = c.execute('SELECT wp_FireRate from filterView').fetchall()
-filFireType = c.execute('SELECT wp_FireType from filterView').fetchall()
-filNoise = c.execute('SELECT wp_noise from filterView').fetchall()
-filterValues =['f_Primary', 'f_Secondary', 'f_Melee', 'f_Electric', 'f_Heat', 'f_Magnetic', 'f_Radiation', 'f_Slash', 'f_Puncture', 'f_Impact', 'f_Rifle', 'f_Shotgun', 'f_Sniper']
-
-ItemViewAndFilters =[[sg.Text('Item View and Filters')], 
-                    [sg.Text('Sort By'), sg.Checkbox('Name', key = 'f_Name'), sg.Checkbox('Damage Type', key = 'f_DamageType'), sg.Checkbox('Fire Rate', key = 'f_FireRate')],
-                    [sg.Text('Filter By: '), sg.Checkbox('Primary', key = 'f_Primary'), sg.Checkbox('Secondary', key = 'f_Secondary'), sg.Checkbox('Melee', key = 'f_Melee')],
-                    [sg.Text('Damge Type '), sg.Checkbox('electric', key = 'f_Electric'), sg.Checkbox('heat', key = 'f_Heat'), sg.Checkbox('magnetic', key = 'f_Magnetic'), sg.Checkbox('radiation', key = 'f_Radiation'), sg.Checkbox('slash', key = 'f_Slash'), sg.Checkbox('puncture', key = 'f_Puncture'), sg.Checkbox('impact', key = 'f_Impact')],
-                    [sg.Text('Weapon Class '), sg.Checkbox('rifle', key = 'f_Rifle'), sg.Checkbox('shotgun', key = 'f_Shotgun'), sg.Checkbox('sniper', key = 'f_Sniper')],
-                    [sg.Button('Update List'), sg.Text('Items: ')], 
-                    [sg.Text('       '), sg.Listbox(filNames, key ='filNames', enable_events=True, size= (15,35), no_scrollbar=True ),
-                     sg.Listbox(filClass, key ='filClass', enable_events=True, size= (15,35), no_scrollbar=True ),
-                     sg.Listbox(filDamageTypes, key ='filDamageTypes', enable_events=True, size= (15,35), no_scrollbar=True ),
-                     sg.Listbox(filFireRate, key ='filFireRate', enable_events=True, size= (15,35), no_scrollbar=True )]]
+layout6 = [[sg.Text('Loadout Viewer', key = 'test1')], [sg.Table(values = testList, headings=testHeadings)]]
 
 weaponEditLayout = [[sg.Button('Back')]]
 
@@ -191,12 +144,11 @@ layout = [[sg.Column(layout1, key='-COL1-'),
            sg.Column(layout5, visible=False, key='-COL5-'),
            sg.Column(layout6, visible=False, key ='-COL6-'),
            sg.Column(weaponEditLayout, visible=False, key ='-EditWeapon-'),
-           sg.Column(warframeEditLayout, visible=False, key ='-EditWarframe-'),
-           sg.Column(ItemViewAndFilters, visible=False, key = '-COL7-')],
-          [sg.Button('1', size = (4, 1)), sg.Button('2', size = (4, 1)), sg.Button('3', size = (4, 1)), sg.Button('4', size = (4, 1)), sg.Button('5', size = (4, 1)), sg.Button('6', size = (4, 1)),sg.Button('7', size = (4, 1)),sg.Button('Exit')]]
+           sg.Column(warframeEditLayout, visible=False, key ='-EditWarframe-')],
+          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'), sg.Button('5'), sg.Button('6'),sg.Button('Exit')]]
 
 
-window = sg.Window('Warframe App', layout, size =(1000,800), resizable=True)
+window = sg.Window('Warframe App', layout, size =(1000,800))
 
 
 
@@ -211,37 +163,15 @@ while True:
         layout = layout + 1 if layout < 6 else 1
         window[f'-COL{layout}-'].update(visible=True)
 
-    elif event in '1234567':
+    elif event in '123456':
         window[f'-COL{layout}-'].update(visible=False)
         layout = int(event)
         window[f'-COL{layout}-'].update(visible=True)
         
         if event == '6':
-            newloadoutNames = c.execute('SELECT l_name FROM loadouts').fetchall()
-            newloadoutPrimaries = c.execute('SELECT l_primaryWeapon FROM loadouts').fetchall()
-            newloadoutSecondaries = c.execute('SELECT l_secondaryWeapon FROM loadouts').fetchall()
-            newloadoutMelee = c.execute('SELECT l_meleeWeapon FROM loadouts').fetchall()
-            newloadoutWarframe = c.execute('SELECT l_warframe FROM loadouts').fetchall()
             result = c.execute('SELECT * FROM weaponsS').fetchall()
-            window['l_primaries'].Update(values=newloadoutPrimaries)
-            window['l_secondaries'].Update(values=newloadoutSecondaries)
-            window['l_melee'].Update(values=newloadoutMelee)
-            window['l_warframes'].Update(values=newloadoutWarframe)
             print(result)
 
-        if event == 7:
-            resetFilterView()
-            
-
-
-    
-    elif event == 'Update List':
-        for i in range(0,13):
-            print(values[(filterValues[i])])
-            if values[(filterValues[i])] == True:
-                filterSelections[i] == True
-            elif values[(filterValues[i])] == False:
-                filterSelections[i] == False
 
 
     elif event == 'UpdateLoadout':
@@ -273,8 +203,13 @@ while True:
         c.execute(query)
         conn.commit()
         sg.popup('Melee', "Melee weapon: " + values['editedWeaponM'] +"\nDamageType: "+ values['DamageTypeM'] +"\nFireRate: "+ values['fireRateM'] + "\nfireType: "+ values['fireTypeM'] + "\nnoise: "+ values['noiseM'])        
-    elif event == 'editedWarframe':
-        pass
+    elif event == 'UpdateWarframeArmor':
+        query = "UPDATE warframe_armor SET wf_id = '"+ values['editedWarframe'] +"',wf_armor = '"+ values['armor'] + "' WHERE wf_id = '"+values['editedWarframe']+ "';"
+        print(query)
+        c.execute(query)
+        conn.commit()
+        sg.popup('Warframe', "Armor: "+ values['armor'])        
+
 
     elif event == 'UpdateWeaponStatsPrimary':
         window[f'-COL2-'].update(visible=False)
@@ -286,19 +221,13 @@ while True:
         window[f'-COL4-'].update(visible=False)
         window[f'-EditWeapon-'].update(visible=True)    
 
-    elif event == 'UpdateWarframe':
+    elif event == 'UpdateWarframeArmor':
         window[f'-COL5-'].update(visible=False)
-        window[f'-EditWeapon-'].update(visible=True)
+        window[f'-EditWarframe-'].update(visible=True)
 
     elif event == 'Back':
         window[f'-EditWeapon-'].update(visible=False)
         window[f'-EditWarframe-'].update(visible=False)
         window[f'-COL2-'].update(visible=True)
 
-    elif event == 'f_Name':
-        filteredResult.sort()
-        window['filteredResult'].Update(filteredResult)
-
 window.close()
-query = "UPDATE weaponsS SET wp_name = '"+ values['editedWeaponS'] +"',wp_DamageTypes = '"+ values['DamageTypeS'] +"', wp_fireRate = '"+ values['fireRateS'] +"', wp_fireType = '"+ values['fireTypeS'] +"', wp_noise = '"+ values['noiseS'] +"' WHERE wp_name = '"+values['editedWeaponS']+ "';"
-query = "UPDATE weaponsM SET wp_name = '"+ values['editedWeaponM'] +"',wp_DamageTypes = '"+ values['DamageTypeM'] +"', wp_fireRate = '"+ values['fireRateM'] +"', wp_fireType = '"+ values['fireTypeM'] +"', wp_noise = '"+ values['noiseM'] +"' WHERE wp_name = '"+values['editedWeaponM']+ "';" 
